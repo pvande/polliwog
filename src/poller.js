f9f405fb6d424e150a420529a867c0bd503cef90
Pieter van de Bruggen <pvande@gmail.com>
2017-07-08 00:37:51 -0700

    Cleaning up `poller` logic somewhat.

--- src/poller.js
+++ src/poller.js
@@ -1,32 +1,31 @@
 module.exports = function(fetchData, emit, options) {
-  let processedBody
   let last = {
-    response: { code: null, body: null },
-    success: { code: null, body: null },
-    failure: { code: null, body: null },
+    response: { code: null, body: null, processed: null },
+    success: { code: null, body: null, processed: null },
+    failure: { code: null, body: null, processed: null },
   }
+
+  const process = body => (options.json ? JSON.parse(body) : body)
 
   return async function() {
     try {
       const { ok, code, headers, body } = await fetchData()
       const responseType = ok ? 'success' : 'failure'
 
-      processedBody = options.json ? JSON.parse(body) : body
+      const newResponseCode = last['response'].code !== code
+      const newResponseBody = last['response'].body !== body
+      const newTypedCode = last[responseType].code !== code
+      const newTypedBody = last[responseType].body !== body
 
-      if (
-        options.emitUnchanged ||
-        last['response'].code !== code ||
-        last['response'].body !== body
-      ) {
-        emit('response', code, headers, processedBody)
+      last['response'].processed = process(body)
+      last[responseType].processed = process(body)
+
+      if (options.emitUnchanged || newResponseCode || newResponseBody) {
+        emit('response', code, headers, last['response'].processed)
       }
 
-      if (
-        options.emitUnchanged ||
-        last[responseType].code !== code ||
-        last[responseType].body !== body
-      ) {
-        emit(responseType, processedBody)
+      if (options.emitUnchanged || newTypedCode || newTypedBody) {
+        emit(responseType, last['response'].processed)
       }
 
       last['response'].code = last[responseType].code = code
