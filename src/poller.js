171612418e89c2ef4180910475ccaf85d51d8740
Pieter van de Bruggen <pvande@gmail.com>
2017-07-08 00:42:39 -0700

    Avoiding redundant body parsing.

--- src/poller.js
+++ src/poller.js
@@ -1,8 +1,8 @@
 module.exports = function(fetchData, emit, options) {
   let last = {
     response: { code: null, body: null, processed: null },
-    success: { code: null, body: null, processed: null },
-    failure: { code: null, body: null, processed: null },
+    success: { body: null, processed: null },
+    failure: { body: null, processed: null },
   }
 
   const process = body => (options.json ? JSON.parse(body) : body)
@@ -10,26 +10,30 @@ module.exports = function(fetchData, emit, options) {
   return async function() {
     try {
       const { ok, code, headers, body } = await fetchData()
-      const responseType = ok ? 'success' : 'failure'
+      const event = ok ? 'success' : 'failure'
 
       const newResponseCode = last['response'].code !== code
       const newResponseBody = last['response'].body !== body
-      const newTypedCode = last[responseType].code !== code
-      const newTypedBody = last[responseType].body !== body
+      const newEventBody = last[event].body !== body
 
-      last['response'].processed = process(body)
-      last[responseType].processed = process(body)
+      last['response'].code = code
+      last['response'].body = last[event].body = body
+
+      if (!newResponseBody) {
+        last[event].processed = last['response'].processed
+      } else if (!newEventBody) {
+        last['response'].processed = last[event].processed
+      } else {
+        last['response'].processed = last[event].processed = process(body)
+      }
 
       if (options.emitUnchanged || newResponseCode || newResponseBody) {
         emit('response', code, headers, last['response'].processed)
       }
 
-      if (options.emitUnchanged || newTypedCode || newTypedBody) {
-        emit(responseType, last['response'].processed)
+      if (options.emitUnchanged || newEventBody) {
+        emit(event, last['response'].processed)
       }
-
-      last['response'].code = last[responseType].code = code
-      last['response'].body = last[responseType].body = body
     } catch (err) {
       emit('error', err)
     } finally {
